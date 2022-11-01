09b1631e1a54ce77e10d49876da9e1e41517939f
Lucas Alves de Souza <mazekeel@gmail.com>
2022-11-01 12:31:35 -0300

    futher customization of the cards layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import './App.css'
 import Card from "./layout/Card"
 import Random from "./basics/Random"
 import Primeiro from "./basics/First"
@@ -8,26 +8,30 @@ import Fragment from "./basics/Fragment"
 
 export default () =>
 
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos react</h1>
-        <Card titulo="Exemplo de Card"></Card>
 
-        <Card titulo="#04 - Random Number">
-            <Random min={1} max={60}/>
-        </Card>
-        
-        <Card titulo="#03 - Fragment">
-            <Fragment />
-        </Card>
+        <div className="Exercises">
 
-        <Card titulo="#02 - Component with parameter">    
-            <ComParametro
-                titulo="Studant Situation"
-                aluno="Pedro"
-                nota={9.3} />
-        </Card>
+            <Card titulo="#04 - Random Number" color="#080">
+                <Random min={1} max={60}/>
+            </Card>
+            
+            <Card titulo="#03 - Fragment" color="#444888">
+                <Fragment />
+            </Card>
 
-        <Card titulo="#01 - First Component">  
-            <Primeiro />
-        </Card>
+            <Card titulo="#02 - Component with parameter" color="#DDDD12
+">    
+                <ComParametro
+                    titulo="Studant Situation"
+                    aluno="Pedro"
+                    nota={9.3} />
+            </Card>
+
+            <Card titulo="#01 - First Component">  
+                <Primeiro />
+            </Card>
+        </div>
+
     </div>
