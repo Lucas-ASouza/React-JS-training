ee6154b68efb0cbcfde910146878308e194ef64f
Lucas Alves de Souza <mazekeel@gmail.com>
2022-11-10 13:03:22 -0300

    futher implemetation of repetition

---
+++ src/data/products.js
@@ -0,0 +1,15 @@
+export default [
+
+    { id: 9, name: 'Pen', price 9.99},
+    { id: 1, name: 'Pencil', price 9.99},
+    { id: 2, name: 'iPad', price 9.99},
+    { id: 3, name: 'Television', price 9.99},
+    { id: 4, name: 'Notebook', price 9.99},
+    { id: 5, name: 'N', price 9.99},
+    { id: 6, name: '', price 9.99},
+    { id: 7, name: '', price 9.99},
+    { id: 8, name: '', price 9.99},
+    { id: 9, name: '', price 9.99},
+
+
+]
