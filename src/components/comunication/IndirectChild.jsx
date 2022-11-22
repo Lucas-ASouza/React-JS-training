5de61d44ec74b89001afcdfa9e6e210e1e7d891a
Lucas Alves de Souza <mazekeel@gmail.com>
2022-11-22 11:07:54 -0300

    Indirect Comunication

--- src/components/comunication/IndirectChild.jsx
+++ src/components/comunication/IndirectChild.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 export default props => {
     const cb = props.onClick
-
+    const min = 30
+    const max = 70
+    const generateAge = () => parseInt(Math.random() * (max-min) + min) /*generate random age*/
+    const generateNerd = () => Math.random() > 0.5 /*generate random Boolean */
     return(
         <div> 
             <div>
                 Child
             </div>
-            <button onClick={_ => cb('John', 42, true)}>
+            <button onClick={_ => cb('John', generateAge, generateNerd)}>
                 Get Information
             </button>
         </div>
--- src/components/comunication/IndirectParent.jsx
+++ src/components/comunication/IndirectParent.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import IndirectChild from "./IndirectChild";
 
 export default props => {
+    const [name, setName] = useState('?')
+    const [age, setAge] = useState(0)
+    const [nerd, setNerd] = useState(false)
+
     
     function getInformation(name, age, nerd) {
-        console.log(name, age, nerd)
+        setName(name)
+        setAge(age)
+        setNerd(nerd)
+
     }
 
     return(
         <div>
-            Parent
-
-            <IndirectChild onClick={getInformation}>
-
-            </IndirectChild>
+            <div>
+                <span>{name} </span>
+                <span><strong>{age}</strong></span>
+                <span>{nerd ? ' and is a True nerd' : ' and is Not a nerd'}</span>
+            </div>
+            <IndirectChild onClick={getInformation}></IndirectChild>
         </div>
     )
 }
